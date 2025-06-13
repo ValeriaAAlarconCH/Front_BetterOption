ef5f200145f7ae8fac34cedc9930216152f220f5
ValeriaAAlarconCH <u20221d218@upc.edu.pe>
2025-06-12 19:42:02 -0500

    Modificaciones 0.1

--- src/app/services/MicroempresaService.ts
+++ src/app/services/MicroempresaService.ts
@@ -2,7 +2,6 @@ import {inject, Injectable} from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {Observable, Subject} from 'rxjs';
-import {Producto} from '../models/producto';
 import {Microempresa} from '../models/microempresa';
 
 @Injectable({
