c639100f3bdca28982f921c90adae6650b6f7f8a
Eduardo <eduardosantillantaboada@gmail.com>
2025-06-14 00:55:19 -0500

    Eduardo

--- src/app/components/catalogopromociones/listarcatalogopromociones/listarcatalogopromociones.ts
+++ src/app/components/catalogopromociones/listarcatalogopromociones/listarcatalogopromociones.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, inject } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import {MatTable, MatTableDataSource} from '@angular/material/table';
+import { Catalogopromociones } from '../../../models/catalogopromociones';
+import { CatalogoPromocionesService } from '../../../services/CatalogoPromocionesService';
 
 @Component({
   selector: 'app-listarcatalogopromociones',
-  imports: [],
   templateUrl: './listarcatalogopromociones.html',
+  imports: [
+    MatPaginator,
+    MatTable
+  ],
   styleUrl: './listarcatalogopromociones.css'
 })
-export class Listarcatalogopromociones {
+export class Listarcatalogopromociones implements OnInit {
+  displayedColumns: string[] = ['id', 'nombre', 'descripcion', 'precio'];
+  dataSource = new MatTableDataSource<Catalogopromociones>();
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  catalogoService = inject(CatalogoPromocionesService);
+
+  ngOnInit(): void {
+    this.catalogoService.listar().subscribe((data) => {
+      this.dataSource.data = data;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
 }
